39db6222e25e4fd5a706aa3a700ee0a20a707b3d
Dmitry Fisenko <fisenkodv@gmail.com>
2019-06-26 21:49:19 -0400

    Update chema

--- src/Dictum.DB/0001-initial-db-schema.sql
+++ src/Dictum.DB/0001-initial-db-schema.sql
@@ -5,9 +5,15 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `authors` (
   `id` int(11) NOT NULL,
-  `uuid` varchar(16) DEFAULT NULL,
-  `name` varchar(128) NOT NULL DEFAULT ''
+  `uuid` varchar(16) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `author_names` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `language_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 CREATE TABLE `languages` (
   `id` int(11) NOT NULL,
@@ -19,6 +25,7 @@ CREATE TABLE `quotes` (
   `id` int(11) NOT NULL,
   `uuid` varchar(16) NOT NULL,
   `text` text NOT NULL,
+  `hash` char(64) NOT NULL,
   `author_id` int(11) NOT NULL,
   `language_id` int(11) NOT NULL,
   `added_at` datetime NOT NULL
@@ -27,20 +34,42 @@ CREATE TABLE `quotes` (
 ALTER TABLE `authors`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `uuid` (`uuid`);
-ALTER TABLE `authors` ADD FULLTEXT KEY `name` (`name`);
+
+ALTER TABLE `author_names`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name_unique` (`name`) USING BTREE,
+  ADD KEY `language_id` (`language_id`) USING BTREE,
+  ADD KEY `author_id` (`author_id`) USING BTREE;
+ALTER TABLE `author_names` ADD FULLTEXT KEY `name_fulltext` (`name`);
 
 ALTER TABLE `languages`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `quotes`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `uuid` (`uuid`),
+  ADD UNIQUE KEY `uuid_unique` (`uuid`) USING BTREE,
+  ADD UNIQUE KEY `hash_unique` (`hash`) USING BTREE,
   ADD KEY `language_id` (`language_id`) USING BTREE,
   ADD KEY `author_id` (`author_id`) USING BTREE;
-ALTER TABLE `quotes` ADD FULLTEXT KEY `text` (`text`);
+ALTER TABLE `quotes` ADD FULLTEXT KEY `text_fulltext` (`text`);
+
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `author_names`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `languages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `quotes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `author_names`
+  ADD CONSTRAINT `author_name_author_foreign_key` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`),
+  ADD CONSTRAINT `author_name_language_foreign_key` FOREIGN KEY (`language_id`) REFERENCES `languages` (`id`);
 
 ALTER TABLE `quotes`
   ADD CONSTRAINT `quote_author_foreign_key` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`),
   ADD CONSTRAINT `quote_language_foreign_key` FOREIGN KEY (`language_id`) REFERENCES `languages` (`id`);
-
 COMMIT;
