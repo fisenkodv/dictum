f41452afabf9d0fb0416842e0f5ac6e621d047ed
Dmitry Fisenko <fisenkodv@gmail.com>
2019-07-15 23:22:05 -0400

    update path

--- src/Quote.Web/src/services/api/quotes-service.ts
+++ src/Quote.Web/src/services/api/quotes-service.ts
@@ -6,7 +6,7 @@ import { baseUrl } from './constants';
 export class QuotesApi {
     public static getRandomQuote(): Promise<Quote> {
         return axios
-            .get(`${baseUrl}/dictum`)
+            .get(`${baseUrl}/quotes`)
             .then(response => response.data)
             .catch(error => console.error(error));
     }
