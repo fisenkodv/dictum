166ba161409b5b152870889a5b1d14631bba3882
Dmitry Fisenko <fisenkodv@gmail.com>
2019-10-13 22:31:04 -0400

    Remove namespace

--- src/Quote.Api/Program.cs
+++ src/Quote.Api/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using JetBrains.Annotations;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
