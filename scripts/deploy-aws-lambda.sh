6eab7d595a0bd97e0be60cb33db9bd8623e00839
Dmitry Fisenko <fisenkodv@gmail.com>
2020-04-10 10:11:04 -0400

    Update deploy-aws-lambda.sh

--- scripts/deploy-aws-lambda.sh
+++ scripts/deploy-aws-lambda.sh
@@ -6,4 +6,4 @@ printf "Building AWS Lambda...\n\n"
 rm function.zip
 npm i
 zip -r function.zip
-aws lambda update-function-code --function-name telegram-expressions-off-mankind-bot --zip-file fileb://function.zip
+aws lambda update-function-code --function-name quote-telegram-bot --zip-file fileb://function.zip
