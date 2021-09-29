a8fc61793f5b65348e237390e1d4dfb22b9df83b
Dmitry Fisenko <fisenkodv@gmail.com>
2021-09-29 17:27:57 -0400

    chore: add jib plugin

--- dictum-service/src/main/java/net/fisenko/dictum/micronaut/Application.java
+++ dictum-service/src/main/java/net/fisenko/dictum/micronaut/Application.java
@@ -1,12 +1,10 @@
 package net.fisenko.dictum.micronaut;
 
 import io.micronaut.runtime.Micronaut;
-import reactor.tools.agent.ReactorDebugAgent;
 
 public class Application {
 
     public static void main(String[] args) {
-        ReactorDebugAgent.init();
         Micronaut.run(Application.class, args);
     }
 }
